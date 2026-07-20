c527c22ed1422aefc59b2c36271bce84072e69ab
Aditya <aditya5j2008y@gmail.com>
2026-07-20 08:35:40 +0530

    Fix paths, handle multiple inputs, and add gitignore

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -3,7 +3,7 @@
 // ****
 // ****
 // ****
-#include <iostream>
+#include <iostream>            // put the value of n= 4 to get above pattern
 using namespace std;
 void print1 ( int n) {                      // don't forget to define i and j data type.
    for ( int i = 0; i<n; i++){               // no variable work without defining them 
@@ -13,10 +13,13 @@ void print1 ( int n) {                      // don't forget to define i and j da
 }
 
 
-
-int main () {
-int n;
-cin>> n;
-print1(n);
-return 0;
+int main() {
+    int n;
+    // This loop keeps running as long as there are numbers to read
+    while (cin >> n) {
+        print1(n);
+        cout << "\n"; // Optional: adds a blank line between different patterns
+    }
+    return 0;
 }
+
