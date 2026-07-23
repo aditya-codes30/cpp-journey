a8ba20db7cc6b296d5624347dd76eeea625325bf
Aditya <aditya5j2008y@gmail.com>
2026-07-23 08:14:21 +0530

    solve this problem again
    i made several mistake in this one

---
+++ patterns/pattern6.cpp
@@ -0,0 +1,29 @@
+// pattern 6 
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+
+
+#include <iostream>
+using namespace std;
+void print6 (int n) {
+    for (int i = 1; i<=n; i++){
+        for( int j = 1; j<= n-i+1; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main (){
+    int n;
+    while(cin>>n){
+        print6(n);
+        cout<<endl;
+    }
+    return 0;
+
+}
